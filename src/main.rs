7e90139801db6c80cca6e45d6e5d6686a0495e62
szhhwh <3116737282@qq.com>
2025-05-11 13:55:14 +0800

    删除日志等级环境变量设置

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,6 @@ mod uploader;
 #[tokio::main]
 async fn main() -> Result<()> {
     // Initialize logging
-    std::env::set_var("RUST_LOG", "trace");
     env_logger::init();
 
     let args = Args::parse();
